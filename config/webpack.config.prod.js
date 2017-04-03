751b1c570da7448ab218ffd622c950e59af88f45
Alexandra Usacheva <lazercore@me.com>
2017-04-03 17:40:16 +0300

    Build fix, add path

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const path = require('path');
 const commonConfig = require('./webpack.config.common');
 
 module.exports = merge(commonConfig, {
