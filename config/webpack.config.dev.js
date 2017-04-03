140b3034908ce68b23f7b957650854c60a7f2c0b
Alexandra Usacheva <lazercore@me.com>
2017-04-03 17:29:51 +0300

    FE-241: webpack new config

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const path = require('path');
+const WriteFilePlugin = require('write-file-webpack-plugin');
+const merge = require('webpack-merge');
+const commonConfig = require('./webpack.config.common');
+
+module.exports = merge(commonConfig, {
+    module: {
+        rules: [
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file-loader?context=./src/&name=../[path][name].[ext]'
+                ],
+                exclude: /node_modules/,
+                include: path.join(__dirname, '../src'),
+            }
+        ]
+    },
+    plugins: [
+        new WriteFilePlugin()
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, '../dist'),
+        compress: true,
+        port: 7050
+    }
+});
