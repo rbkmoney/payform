42512468aa871e8c7d2a0b81e3c30224fbc11076
Alexandra Usacheva <lazercore@me.com>
2017-06-01 17:48:28 +0300

    FE-267: check accessToken

--- src/utils/CheckIntegration.js
+++ src/utils/CheckIntegration.js
@@ -1,5 +1,6 @@
 import Matcher from 'did-you-mean';
 import integration from './dictionary';
+import jwtDecode from 'jwt-decode';
 
 export default class CheckIntegration {
     static getMatcher() {
@@ -17,10 +18,51 @@ export default class CheckIntegration {
         return m;
     }
 
+    static checkToken(key, invoiceID) {
+        const decodedKey = jwtDecode(key);
+        if (decodedKey.resource_access && decodedKey.resource_access['common-api'] && decodedKey.resource_access['common-api'].roles) {
+            const arr = decodedKey.resource_access['common-api'].roles;
+            const roles = {};
+
+            for (let i = 0; i < arr.length; i++) {
+                const splited = arr[i].split(':');
+                if (!roles.hasOwnProperty([splited[0]])) {
+                    roles[splited[0]] = splited[1];
+                } else {
+                    roles[splited[0]] = {
+                        [roles[splited[0]]]: true,
+                        [splited[1]]: true
+                    }
+                }
+            }
+
+            const invoicesRead = roles[`invoices.${invoiceID}`] === 'read';
+            const invoicesPayments = roles[`invoices.${invoiceID}.payments`] && roles[`invoices.${invoiceID}.payments`].read && roles[`invoices.${invoiceID}.payments`].write;
+            const paymentTool = roles['payment_tool_tokens'] === 'write';
+
+            if (invoicesRead && invoicesPayments && paymentTool) {
+                return true;
+            } else {
+                console.error('RbkmoneyCheckout.configure: RbkmoneyCheckout.configure: Wrong value \'data-invoice-access-token\'. You used API key. Please use invoice access token.');
+                return false;
+            }
+        }
+    }
+
     static check(props) {
         const m = CheckIntegration.getMatcher();
         let errors = 0;
         let criticalErrors = 0;
+
+        if (props.invoiceAccessToken && props.invoiceID) {
+            if (!CheckIntegration.checkToken(props.invoiceAccessToken, props.invoiceID)) {
+                ++criticalErrors;
+            }
+        }
+
+        if (criticalErrors > 0) {
+            alert('RbkmoneyCheckout.configure: Critical error! Check your console for more info.');
+        }
 
         for (const prop in props) {
             if (props.hasOwnProperty(prop)) {
@@ -40,7 +82,7 @@ export default class CheckIntegration {
             }
         }
 
-        if (errors > 0) {
+        if (errors > 0 || criticalErrors > 0) {
             console.warn('You can learn about the available configuration options in the Checkout docs: https://rbkmoney.github.io/docs/integrations/checkout');
         }
 
