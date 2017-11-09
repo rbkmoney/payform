356d4211829adce7a594d8661ca07a6334c49b79
Alexandra Usacheva <lazercore@me.com>
2017-11-09 14:30:11 +0300

    FE-499: overlay loading anim (#136)
    * FE-499: overlay loading anim

--- src/app/components/container/container.tsx
+++ src/app/components/container/container.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import * as styles from './container.scss';
 
-import {Header, Info, Footer, Form, ThreeDSContainer} from '../index';
+import {Header, Info, Footer, Form, ThreeDSContainer, ContainerLoader} from '../index';
 
 export class Container extends React.Component {
     static getView(): string {
         switch (window.location.search) {
+            case '?view=loading':
+                return 'loading';
             case '?view=3ds':
                 return '3ds';
             case '?view=default':
@@ -17,14 +19,15 @@ export class Container extends React.Component {
     render() {
         return (
             <div className={styles.container}>
-                <div className={styles.form_container}>
-                    {Container.getView() === 'default' ? <Header/> : false}
-                    {Container.getView() === 'default' ? <Info/> : false}
-                    {Container.getView() === 'default' ? <Form/> : false}
-                    {Container.getView() === 'default' ? <Footer/> : false}
-                    {Container.getView() === '3ds' ? <ThreeDSContainer/> : false}
-                </div>
-                <Footer/>
+                {Container.getView() !== 'loading' ?
+                    <div className={styles.form_container}>
+                        {Container.getView() === 'default' ? <Header/> : false} {Container.getView() === 'default' ?
+                        <Info/> : false} {Container.getView() === 'default' ?
+                        <Form/> : false} {Container.getView() === 'default' ?
+                        <Footer/> : false} {Container.getView() === '3ds' ? <ThreeDSContainer/> : false}
+                    </div>
+                    : <ContainerLoader/> }
+                    {Container.getView() !== 'loading' ? <Footer/> : false}
             </div>
 
         );
