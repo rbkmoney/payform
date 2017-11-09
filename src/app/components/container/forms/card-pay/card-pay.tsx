bf947230a15066f64685583db6c0b2d25f338a53
Alexandra Usacheva <lazercore@me.com>
2017-11-09 15:12:23 +0300

    FE-498: added form header (#137)
    * FE-498: added form header

--- src/app/components/container/forms/card-pay/card-pay.tsx
+++ src/app/components/container/forms/card-pay/card-pay.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
 import * as styles from './card-pay.scss';
 import * as formStyles from '../forms.scss';
-import {Input, Button} from '../../../index';
+import {Input, Button, Icon} from '../../../index';
 
 export class CardPay extends React.Component {
     render() {
         return (
             <form>
-                <div className={formStyles.title}>
-                    Оплата банковской картой
+                <div className={formStyles.header}>
+                    <div className={formStyles.back_btn}>
+                        <Icon icon='chevron-left'/>
+                    </div>
+                    <div className={formStyles.title}>
+                        Оплата банковской картой
+                    </div>
                 </div>
                 <div className='form-group'>
                     <Input icon='card' placeholder='Номер на карте'/>
