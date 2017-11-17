5d9383e1526b1dba90213dc1c08d913c01e7036a
Ildar Galeev <KeinAsylum@gmail.com>
2017-11-17 16:17:26 +0300

    Refactoring.

--- src/payframe/components/isInvoicePaymentAvailable.js
+++ src/payframe/components/isInvoicePaymentAvailable.js
@@ -1,27 +1,19 @@
 import toNumber from 'lodash/toNumber';
 
 function isInvoicePaymentAvailable(props) {
-    const isInvoiceWithTemplateCreated = props.integration.invoiceAccessToken;
-    if (isInvoiceWithTemplateCreated) {
-        const invoiceTemplateDetails = props.integration.invoiceTemplate.details;
-        const templateType = invoiceTemplateDetails.templateType;
-        switch (templateType) {
-            case 'InvoiceTemplateSingleLine': {
-                const costType = invoiceTemplateDetails.price.costType;
-                if (costType !== 'InvoiceTemplateLineCostFixed') {
-                    const formAmount = toNumber(props.viewData.cardForm.amount.value) * 100;
-                    const invoiceAmount = props.integration.invoice.amount;
-                    return (formAmount === invoiceAmount);
-                } else {
-                    return true;
-                }
-            }
-            case 'InvoiceTemplateMultiLine':
-                return true;
-        }
-    } else {
+    if (!props.integration.invoiceAccessToken) {
         return false;
     }
+    const details = props.integration.invoiceTemplate.details;
+    const isSingeLine = details.templateType === 'InvoiceTemplateSingleLine';
+    const isNotFixed = details.price && details.price.costType !== 'InvoiceTemplateLineCostFixed';
+    let result = true;
+    if (isSingeLine && isNotFixed) {
+        const formAmount = toNumber(props.viewData.cardForm.amount.value) * 100;
+        const invoiceAmount = props.integration.invoice.amount;
+        result = (formAmount === invoiceAmount);
+    }
+    return result;
 }
 
 export default isInvoicePaymentAvailable;
