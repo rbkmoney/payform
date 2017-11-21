b1deb76944446733ffed15271cd5b3b4023ce4ac
Alexandra Usacheva <lazercore@me.com>
2017-11-21 15:54:47 +0300

    FE-506: 3ds animations (#148)
    * FE-506: 3ds animations

--- src/app/components/container/container-loader/container-loader.scss.d.ts
+++ src/app/components/container/container-loader/container-loader.scss.d.ts
@@ -2,4 +2,6 @@ export const loader: string;
 export const growth: string;
 export const fadein: string;
 export const popup: string;
+export const rotatein: string;
+export const rotateout: string;
 export const shake: string;
