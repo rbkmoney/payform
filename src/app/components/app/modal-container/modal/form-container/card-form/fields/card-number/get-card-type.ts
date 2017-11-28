e45669af71906e08b0c888533d3b32f678ce0bdf
Alexandra Usacheva <lazercore@me.com>
2017-11-28 18:24:02 +0300

    Update get-card-type.ts

--- src/app/components/app/modal-container/modal/form-container/card-form/fields/card-number/get-card-type.ts
+++ src/app/components/app/modal-container/modal/form-container/card-form/fields/card-number/get-card-type.ts
@@ -1,18 +1,5 @@
 import * as creditCardType from 'credit-card-type';
-
-type CardBrand = 'american-express' | 'diners-club' | 'discover' | 'jcb' | 'maestro' | 'master-card' | 'unionpay' | 'visa';
-interface CreditCardTypeInfo {
-        niceType?: string;
-        type?: CardBrand;
-        prefixPattern?: RegExp;
-        exactPattern?: RegExp;
-        gaps?: number[];
-        lengths?: number[];
-        code?: {
-            name?: string;
-            size?: number;
-        };
-    }
+import {CreditCardTypeInfo} from 'credit-card-type';
 
 export function getCardType(cardNumber: string): CreditCardTypeInfo {
     return creditCardType(cardNumber)[0];
