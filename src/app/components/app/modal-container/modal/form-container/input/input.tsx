a7b6913315f42787bcea7062361991a07dad16ec
Alexandra Usacheva <lazercore@me.com>
2017-11-29 15:37:05 +0300

    FE-516: PR FIX

--- src/app/components/app/modal-container/modal/form-container/input/input.tsx
+++ src/app/components/app/modal-container/modal/form-container/input/input.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as styles from './input.scss';
 import * as cx from 'classnames';
-import {IconType, Icon} from '../../../../../ui/icon';
+import { IconType, Icon } from '../../../../../ui/icon';
 
 interface InputProps {
     icon?: IconType;
