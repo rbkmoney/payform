b1cf97957505b78636e1975fda82016aee9477fa
Alexandra Usacheva <lazercore@me.com>
2017-12-11 17:25:34 +0300

    FE-518: PR fix

--- src/app/components/app/modal-container/modal/form-container/input/marks/marks.tsx
+++ src/app/components/app/modal-container/modal/form-container/input/marks/marks.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import * as styles from 'checkout/components/app/modal-container/modal/form-container/input/input.scss';
-import { IconType, Icon } from 'checkout/components/ui';
+import { Icon, IconType } from 'checkout/components/ui';
 
 interface MarksProps {
     active: boolean;
@@ -11,20 +11,21 @@ interface MarksProps {
 
 export const Marks: React.SFC<MarksProps> = (props) => (
     <CSSTransitionGroup
-            component='div'
-            transitionName={{
-                appear: styles.appearMarks,
-                enter: styles.enterMarks,
-                leave: styles.leaveMarks
-            }}
-            transitionEnterTimeout={500}
-            transitionLeaveTimeout={500}
-            transitionAppearTimeout={500}
-            transitionAppear={true}
-            transitionEnter={true}
-            transitionLeave={true}
-        >
-            {!props.active && !props.error && !props.pristine ? <Icon className={styles.checkmark} icon={IconType.checkmark}/> : false}
-            {!props.active && props.error ? <Icon className={styles.errorCross} icon={IconType.redCross}/> : false }
-        </CSSTransitionGroup>
+        component='div'
+        transitionName={{
+            appear: styles.appearMarks,
+            enter: styles.enterMarks,
+            leave: styles.leaveMarks
+        }}
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={500}
+        transitionAppearTimeout={500}
+        transitionAppear={true}
+        transitionEnter={true}
+        transitionLeave={true}
+    >
+        {!props.active && !props.error && !props.pristine ?
+            <Icon className={styles.checkmark} icon={IconType.checkmark}/> : false}
+        {!props.active && props.error ? <Icon className={styles.errorCross} icon={IconType.redCross}/> : false}
+    </CSSTransitionGroup>
 );
