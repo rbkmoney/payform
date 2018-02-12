ba06d336f2cd201df662a127ed84b3004289ffff
Ildar Galeev <KeinAsylum@gmail.com>
2018-02-12 14:58:34 +0300

    FE-556: Added test message.

--- src/app/log-messages.ts
+++ src/app/log-messages.ts
@@ -1,4 +1,3 @@
 export const logPrefix = '[RbkmoneyCheckout]';
 export const sadnessMessage = 'Param will not be applied.';
-export const getMessageInvalidValue = (fieldName: string, value: string, reason: string): string =>
-    `${logPrefix} Invalid value of param '${fieldName}':'${value}'. ${reason} ${sadnessMessage}`;
+export const getMessageInvalidValue = (fieldName: string, value: string, reason: string): string => 'TEST';
