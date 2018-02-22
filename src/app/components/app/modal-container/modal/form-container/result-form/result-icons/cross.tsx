429f6ed928ce22229241c99055430a1230b648c6
Ildar Galeev <KeinAsylum@gmail.com>
2018-02-22 14:15:13 +0300

    FE-562: Svg optimization. (#216)

--- src/app/components/app/modal-container/modal/form-container/result-form/result-icons/cross.tsx
+++ src/app/components/app/modal-container/modal/form-container/result-form/result-icons/cross.tsx
@@ -2,9 +2,8 @@ import * as React from 'react';
 import * as styles from './cross.scss';
 
 export const Cross: React.SFC = () => (
-    <svg className={styles.cross} viewBox='0 0 50 50' version='1.1' id='cross-icon'>
+    <svg className={styles.cross} viewBox='0 0 50 50'>
         <circle cx='26' cy='26' r='25' fill='none'/>
-        <path d='M36,36.8502994 L13.5,13' strokeLinecap='square'/>
-        <path d='M14,37 L36,13' strokeLinecap='square'/>
+        <path d='M36 36.85L13.5 13M14 37l22-24'/>
     </svg>
 );
