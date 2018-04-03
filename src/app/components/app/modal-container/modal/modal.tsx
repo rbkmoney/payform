f8c5f200b9f1395373faccaadf21dd2db424e46e
Alexandra Usacheva <lazercore@me.com>
2018-04-03 17:00:21 +0300

    FE-585: added overlay background for popupmode

--- src/app/components/app/modal-container/modal/modal.tsx
+++ src/app/components/app/modal-container/modal/modal.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import * as CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 import * as styles from './modal.scss';
+import * as cx from 'classnames';
 import { Info } from './info';
 import { Footer } from '../footer';
 import { MobileHeader } from './mobile-header';
 import { FormContainer } from './form-container';
+import {State} from 'checkout/state';
+import {connect} from 'react-redux';
 
-const ModalDef: React.SFC = () => (
+interface ModalDefProps {
+    popupMode: boolean;
+}
+
+const ModalDef: React.SFC<ModalDefProps> = (props) => (
     <CSSTransitionGroup
         component='div'
         transitionName={{
@@ -21,7 +28,9 @@ const ModalDef: React.SFC = () => (
         transitionEnter={true}
         transitionLeave={true}
     >
-        <div className={styles.form_container} id='form-container'>
+        <div className={cx(styles.form_container, {
+            [styles.with_shadow]: props.popupMode
+        })} id='form-container'>
             <MobileHeader/>
             <Info/>
             <FormContainer/>
@@ -30,4 +39,8 @@ const ModalDef: React.SFC = () => (
     </CSSTransitionGroup>
 );
 
-export const Modal = ModalDef;
+const mapStateToProps = (state: State) => ({
+    popupMode: state.config.initConfig.popupMode
+});
+
+export const Modal = connect(mapStateToProps)(ModalDef);
