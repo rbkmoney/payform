47878fbc82c6e69e17c3ccb97ea6f1f047bded02
Ildar Galeev <keinasylum@gmail.com>
2018-04-04 15:29:59 +0300

    Used AsYouType.

--- src/app/components/app/modal-container/modal/form-container/common-fields/phone/format-phone-number.ts
+++ src/app/components/app/modal-container/modal/form-container/common-fields/phone/format-phone-number.ts
@@ -1,5 +1,5 @@
 import { FormEvent } from 'react';
-import { format } from 'libphonenumber-js/custom';
+import { AsYouType } from 'libphonenumber-js/custom';
 
 import * as metadata from './metadata.json';
 
@@ -7,8 +7,8 @@ export const formatPhoneNumber = (e: FormEvent<HTMLInputElement>) => {
     const target = e.currentTarget as HTMLInputElement;
     const value = target.value;
     if (value.slice(0, 1) === '+') {
-        target.value = format(value, 'International', metadata);
+        target.value = new AsYouType('RU', metadata).input(value);
     } else {
-        target.value = `+`;
+        target.value = '+';
     }
 };
