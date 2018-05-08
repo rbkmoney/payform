e52ecbabb1c20376cebe56def768defc2fa72449
Ildar Galeev <keinasylum@gmail.com>
2018-05-08 16:22:56 +0300

    Up to date with master.

--- src/app/sagas/payment/provide-payment/pay-with-apple-pay/validate-merchant.ts
+++ src/app/sagas/payment/provide-payment/pay-with-apple-pay/validate-merchant.ts
@@ -1,6 +1,6 @@
 export const validateMerchant = (endpoint: string, payload: ApplePayPayload, validationURL: string): Promise<any> => {
     return new Promise((resolve, reject) => {
-        fetch(`${endpoint}/applepay/api/v1/session`, {
+        fetch(`${endpoint}/api/v1/session`, {
             method: 'POST',
             body: JSON.stringify({
                 merchantId: payload.merchantIdentifier,
