428e7eeccaf884c8746b7252496ec800dea8c1bd
Ildar Galeev <keinasylum@gmail.com>
2018-05-10 11:48:04 +0300

    Up to date with master.

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,14 @@
 import { Initializer } from './initializer/initializer';
+import { logPrefix } from 'checkout/log-messages';
+
+export const isApplePayAvailable = (): boolean => {
+    try {
+        return environment.ApplePaySession && ApplePaySession.canMakePayments();
+    } catch (e) {
+        console.warn(`${logPrefix} isApplePayAvailable`, e);
+        return false;
+    }
+};
 
 export interface Environment extends Window {
     RbkmoneyCheckout?: Configurator;
