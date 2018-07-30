d061f533cf4c1d99bfe077e7c21eecb3202ce931
Rinat Arsaev <krickray@gmail.com>
2018-07-30 15:41:57 +0300

    to async-await

--- src/app/backend/fetch-wrapper.ts
+++ src/app/backend/fetch-wrapper.ts
@@ -6,21 +6,22 @@ export class FetchWrapperParams {
     body?: any;
 }
 
-export function fetchWrapper<T>(param: FetchWrapperParams): Promise<T> {
-    return new Promise((resolve, reject) => {
-        fetch(param.endpoint, {
+export async function fetchWrapper<T>(param: FetchWrapperParams): Promise<T> {
+    try {
+        const res = await fetch(param.endpoint, {
             method: param.method || 'GET',
             headers: {
                 'Content-Type': 'application/json;charset=utf-8',
                 'X-Request-ID': guid()
             },
             body: param.body ? JSON.stringify(param.body) : undefined
-        }).then((res) =>
-            res.status >= 200 && res.status <= 300
-                ? resolve(res.json())
-                : res.json()
-                    .then((ex) => reject(ex))
-                    .catch(() => reject({code: res.status}))
-        ).catch((ex) => reject({code: `${ex}`}));
-    });
+        });
+        if (res.status >= 200 && res.status <= 300) {
+            return await res.json();
+        } else {
+            throw {code: res.status};
+        }
+    } catch (e) {
+        throw e;
+    }
 }
