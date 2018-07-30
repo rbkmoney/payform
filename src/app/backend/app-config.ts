e5e75824afe604f7efbabc26eff9225ed3648e21
Rinat Arsaev <krickray@gmail.com>
2018-07-30 17:34:17 +0300

    remove applePayMerchantValidationEndpoint

--- src/app/backend/app-config.ts
+++ src/app/backend/app-config.ts
@@ -2,7 +2,6 @@ export class AppConfig {
     capiEndpoint: string;
     wrapperEndpoint: string;
     applePayMerchantID: string;
-    applePayMerchantValidationEndpoint: string;
     googlePayMerchantID: string;
     googlePayGatewayMerchantID: string;
     samsungPayMerchantName: string;
