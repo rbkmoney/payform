2f804faa4a71c75462d1866480b826e97a744e7f
Rinat Arsaev <krickray@gmail.com>
2018-08-01 19:34:45 +0300

    upd checkmark icon

--- src/app/components/app/modal-container/modal/form-container/result-form/result-icons/checkmark.tsx
+++ src/app/components/app/modal-container/modal/form-container/result-form/result-icons/checkmark.tsx
@@ -2,8 +2,16 @@ import * as React from 'react';
 import * as styles from './checkmark.scss';
 
 export const Checkmark: React.SFC = () => (
-    <svg id="checkmark-icon" className={styles.checkmark} viewBox="0 0 52 52">
-        <circle cx="26" cy="26" r="25" fill="none" />
-        <path fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
+    <svg id="checkmark-icon" className={styles.checkmark} viewBox="0 0 86 86">
+        <g fillRule="nonzero" fill="none">
+            <circle fill="#FFF" cx="43" cy="43" r="43" />
+            <path
+                stroke="#FFF"
+                strokeWidth="5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M27 42.862L39.089 55 61 33"
+            />
+        </g>
     </svg>
 );
