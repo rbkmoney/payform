5e37204289b34c00564f738184a5f05c9d4d6a7b
Rinat Arsaev <krickray@gmail.com>
2018-08-02 13:42:18 +0300

    Add samsung pay callback URL (#267)

--- src/app/sagas/payment/provide-payment/pay-with-samsung-pay/create-transaction.ts
+++ src/app/sagas/payment/provide-payment/pay-with-samsung-pay/create-transaction.ts
@@ -2,9 +2,16 @@ import { Transaction } from 'checkout/backend/model';
 import { guid } from 'checkout/utils';
 import { AmountInfoState } from 'checkout/state';
 import { AppConfig } from 'checkout/backend';
+import { URIPath } from '../../../../../constants/samsung-pay-communicator';
 
-const getTransactionRequestBody = (totalAmount: number, currency: string, merchantName: string, serviceID: string) => ({
-    callback: '0',
+const getTransactionRequestBody = (
+    totalAmount: number,
+    currency: string,
+    merchantName: string,
+    serviceID: string,
+    callbackURL: string
+) => ({
+    callback: callbackURL,
     paymentDetails: {
         service: {
             id: serviceID
@@ -27,7 +34,14 @@ const getTransactionRequestBody = (totalAmount: number, currency: string, mercha
 export async function createTransaction(appConfig: AppConfig, a: AmountInfoState): Promise<Transaction> {
     const { minorValue, currencyCode } = a;
     const { samsungPayMerchantName, samsungPayServiceID, wrapperEndpoint } = appConfig;
-    const body = getTransactionRequestBody(minorValue / 100, currencyCode, samsungPayMerchantName, samsungPayServiceID);
+    const callbackURL = `${window.location.origin}${URIPath}`;
+    const body = getTransactionRequestBody(
+        minorValue / 100,
+        currencyCode,
+        samsungPayMerchantName,
+        samsungPayServiceID,
+        callbackURL
+    );
     try {
         const res = await fetch(`${wrapperEndpoint}/samsungpay/api/v1/transaction`, {
             method: 'POST',
