5d1ae150821142d51a0bf8e515419a69ee894180
Rinat Arsaev <krickray@gmail.com>
2018-08-08 11:57:51 +0300

    fix slide animation

--- src/app/components/app/modal-container/modal/form-container/form-container.tsx
+++ src/app/components/app/modal-container/modal/form-container/form-container.tsx
@@ -54,35 +54,55 @@ class FormContainerDef extends React.Component<FormContainerProps> {
                 <div
                     className={cx(styles.form, { [styles._error]: viewInfo.error })}
                     style={{ height: viewInfo.height || 'auto' }}>
-                    <CSSTransitionGroup
-                        component="div"
-                        className={styles.animationFormContainer}
-                        transitionName={viewInfo.slideDirection}
-                        transitionEnterTimeout={550}
-                        transitionLeaveTimeout={550}>
-                        <div ref={this.setContentElement} key={name}>
-                            {name === FormName.paymentMethods ? <PaymentMethods /> : null}
-                            {name === FormName.cardForm ? <CardForm /> : null}
-                            {name === FormName.walletForm ? <WalletForm /> : null}
-                            {name === FormName.terminalForm ? <TerminalForm /> : null}
-                            {name === FormName.resultForm ? <ResultForm /> : null}
-                            {name === FormName.helpForm ? <Help /> : null}
-                            {name === FormName.interactionForm ? <InteractionForm /> : null}
-                            {name === FormName.tokenProviderForm ? <TokenProviderForm /> : null}
-                        </div>
-                    </CSSTransitionGroup>
-                    {viewInfo.inProcess ? <FormLoader /> : null}
+                    <div ref={this.setContentElement}>
+                        <CSSTransitionGroup
+                            component="div"
+                            className={styles.animationFormContainer}
+                            transitionName={viewInfo.slideDirection}
+                            transitionEnterTimeout={500}
+                            transitionLeaveTimeout={500}
+                            onTransitionEnd={this.setHeight}>
+                            <div key={name}>{this.renderForm(name)}</div>
+                        </CSSTransitionGroup>
+                        {!!viewInfo.inProcess && <FormLoader />}
+                    </div>
                 </div>
             </div>
         );
     }
+
+    private renderForm = (name: FormName): React.ReactNode => {
+        switch (name) {
+            case FormName.paymentMethods:
+                return <PaymentMethods />;
+            case FormName.cardForm:
+                return <CardForm />;
+            case FormName.walletForm:
+                return <WalletForm />;
+            case FormName.terminalForm:
+                return <TerminalForm />;
+            case FormName.resultForm:
+                return <ResultForm />;
+            case FormName.helpForm:
+                return <Help />;
+            case FormName.interactionForm:
+                return <InteractionForm />;
+            case FormName.tokenProviderForm:
+                return <TokenProviderForm />;
+            default:
+                return null;
+        }
+    };
 
     private setContentElement = (element: HTMLDivElement) => {
         this.contentElement = element;
     };
 
     private setHeight = () => {
-        this.props.setViewInfoHeight(this.contentElement ? this.contentElement.clientHeight : 0);
+        const height = this.contentElement ? this.contentElement.clientHeight : 0;
+        if (height !== this.props.viewInfo.height) {
+            this.props.setViewInfoHeight(height);
+        }
     };
 }
 
