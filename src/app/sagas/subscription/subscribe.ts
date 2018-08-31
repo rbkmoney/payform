58d32496a17e2ef4a40d1bc109b467f7d9cc6619
Rinat Arsaev <krickray@gmail.com>
2018-08-31 11:44:34 +0300

    fix subscribe

--- src/app/sagas/subscription/subscribe.ts
+++ src/app/sagas/subscription/subscribe.ts
@@ -5,24 +5,34 @@ import {
     TypeKeys,
     SubscriptionRequested,
     SubscriptionCompleted,
-    SubscriptionFailed
+    SubscriptionFailed,
+    goToFormInfo,
+    GoToFormInfo
 } from 'checkout/actions';
-import { State } from 'checkout/state';
+import { EventsStatus, ResultFormInfo, ResultType, State } from 'checkout/state';
 import { provideSubscription } from './provide-subscription';
 import { provideFromCustomerEvent } from '../provide-modal';
 
 type SubscribePutEffect = PrepareToPay | SubscriptionCompleted | SubscriptionFailed;
 
-type SubscribeEffect = SelectEffect | CallEffect | PutEffect<SubscribePutEffect>;
+type SubscribeEffect = SelectEffect | CallEffect | PutEffect<SubscribePutEffect | GoToFormInfo>;
 
 export function* subscribe(action: SubscriptionRequested): Iterator<SubscribeEffect> {
     try {
         const config = yield select((s: State) => s.config);
         yield put({ type: TypeKeys.PREPARE_TO_PAY } as PrepareToPay);
         yield call(provideSubscription, config, action.payload);
-        const event = yield select((s: State) => last(s.events.customerEvents));
-        yield call(provideFromCustomerEvent, event);
-        yield put({ type: TypeKeys.SUBSCRIPTION_COMPLETED } as SubscriptionCompleted);
+        const customerEventsStatus = yield select((state: State) => state.events.customerEventsStatus);
+        switch (customerEventsStatus) {
+            case EventsStatus.polled:
+                const event = yield select((s: State) => last(s.events.customerEvents));
+                yield call(provideFromCustomerEvent, event);
+                yield put({ type: TypeKeys.SUBSCRIPTION_COMPLETED } as SubscriptionCompleted);
+                break;
+            case EventsStatus.timeout:
+                yield put(goToFormInfo(new ResultFormInfo(ResultType.processed)));
+                break;
+        }
     } catch (error) {
         yield put({
             type: TypeKeys.SUBSCRIPTION_FAILED,
