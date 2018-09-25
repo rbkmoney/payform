9c098b1e1f2e3eff35c71a492928ebaaeb5af989
Rinat Arsaev <krickray@gmail.com>
2018-09-25 17:22:08 +0300

    revert

--- src/app/utils/get-url-params.ts
+++ src/app/utils/get-url-params.ts
@@ -4,7 +4,7 @@ interface URLParams {
     [param: string]: boolean | string | number | null | undefined;
 }
 
-export const getUrlParams = (url: string, isOnlyParams: boolean = false): URLParams => {
+export const getUrlParams = (url: string): URLParams => {
     const params: URLParams = {};
     if (url && typeof url === 'string') {
         let decodedUrl;
@@ -15,8 +15,8 @@ export const getUrlParams = (url: string, isOnlyParams: boolean = false): URLPar
             decodedUrl = url;
         }
         const urlParts = splitByFirst(decodedUrl, '?');
-        const paramsStr = urlParts[isOnlyParams ? 0 : 1];
-        if (paramsStr) {
+        if (urlParts[1]) {
+            const paramsStr = urlParts[1];
             const paramsPartsStr = paramsStr.split('&');
             for (const paramStr of paramsPartsStr) {
                 const [name, value] = splitByFirst(paramStr, '=') as [string, string | undefined];
