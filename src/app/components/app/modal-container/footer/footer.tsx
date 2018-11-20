7860cb7d6018a2d9e5f77ec74b076e4bca9136ff
Rinat Arsaev <krickray@gmail.com>
2018-11-20 18:02:55 +0300

    brandable footer

--- src/app/components/app/modal-container/footer/footer.tsx
+++ src/app/components/app/modal-container/footer/footer.tsx
@@ -3,6 +3,7 @@ import * as styles from './footer.scss';
 import { connect } from 'react-redux';
 import { State } from 'checkout/state';
 import { Locale } from 'checkout/locale';
+import { Config } from 'checkout/config';
 import { SecureIcon } from './secure-icon';
 import { VisaIcon } from './visa-icon';
 import { McIcon } from './mc-icon';
@@ -11,20 +12,24 @@ import { Logo } from './logo';
 
 export interface FooterProps {
     locale: Locale;
+    config: Config;
 }
 
 const mapStateToProps = (state: State) => ({
-    locale: state.config.locale
+    locale: state.config.locale,
+    config: state.config
 });
 
 const FooterDef: React.SFC<FooterProps> = (props) => (
     <footer className={styles.footer}>
         <div className={styles.safe_payment_container}>
-            {/*<div className={styles.safe_payment}>*/}
-            {/*<SecureIcon className={styles.secure_icon} />*/}
-            {/*<p className={styles.label}>{props.locale['footer.pay.label']}</p>*/}
-            {/*<Logo />*/}
-            {/*</div>*/}
+            {!props.config.appConfig.notBranded && (
+                <div className={styles.safe_payment}>
+                    <SecureIcon className={styles.secure_icon} />
+                    <p className={styles.label}>{props.locale['footer.pay.label']}</p>
+                    <Logo />
+                </div>
+            )}
             <div className={styles.safe_logos}>
                 <VisaIcon fillStyle={styles.fill_icons} />
                 <McIcon className={styles.align_fix} fillStyle={styles.fill_icons} />
