ed6a184f14ffe14a8a1f10329e434d5309c6e99c
Rinat Arsaev <krickray@gmail.com>
2018-11-23 14:01:20 +0300

    add crab success icon

--- src/app/components/app/modal-container/modal/form-container/result-form/result-icons/success-icon/success-icon.tsx
+++ src/app/components/app/modal-container/modal/form-container/result-form/result-icons/success-icon/success-icon.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
 
 import Success from './success-icon.svg';
-import styled from 'checkout/styled-components';
+import NeutralSuccess from './neutral-success-icon.svg';
+import styled, { withTheme } from 'checkout/styled-components';
+import { themes } from 'checkout/themes';
+import Theme from 'checkout/themes/theme';
 
-export const SuccessIcon = styled(Success).attrs({ id: 'success-icon' })`
+export const SuccessIcon = styled(
+    withTheme(({ theme, ...props }: { theme: Theme }) => {
+        const Icon = theme.name === themes.main.name ? Success : NeutralSuccess;
+        return <Icon {...props} id="success-icon" />;
+    })
+)`
     width: 100px;
     height: 100px;
     display: block;
