25cf13ddee499e467af65d036238468578532bd0
Rinat Arsaev <krickray@gmail.com>
2018-11-23 14:08:16 +0300

    fix error-icon

--- src/app/components/app/modal-container/modal/form-container/result-form/result-icons/error-icon/error-icon.tsx
+++ src/app/components/app/modal-container/modal/form-container/result-form/result-icons/error-icon/error-icon.tsx
@@ -1,21 +1,29 @@
 import * as React from 'react';
 
-import styled from 'checkout/styled-components';
+import styled, { css, withTheme } from 'checkout/styled-components';
 import { growth } from 'checkout/styled-components/animations';
-import Icon from './error-icon.svg';
+import Error from './error-icon.svg';
+import NeutralError from './neutral-error-icon.svg';
+import { themes } from 'checkout/themes';
+import Theme from 'checkout/themes/theme';
 
-export const ErrorIcon = styled((props) => <Icon {...props} id="error-icon" />)`
+export const ErrorIcon = styled(
+    withTheme(({ theme, ...props }: { theme: Theme }) => {
+        const Icon = theme.name === themes.main.name ? Error : NeutralError;
+        return <Icon {...props} id="error-icon" />;
+    })
+)`
     width: 100px;
     height: 100px;
     display: block;
     margin: auto;
 
-    g circle {
-        // fill: ${({ theme }) => theme.color.primary[1]};
-    }
-
-    g path {
-        transform-origin: 30% 0%;
-        animation: ${growth} 0.5s linear 0.2s forwards;
-    }
+    ${({ theme }) =>
+        theme.name === themes.main.name &&
+        css`
+            g path {
+                transform-origin: 30% 0%;
+                animation: ${growth} 0.5s linear 0.2s forwards;
+            }
+        `}
 `;
