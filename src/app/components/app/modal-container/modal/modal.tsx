2a918f7910da73721d2cfc12fcd5950773110ea7
Rinat Arsaev <krickray@gmail.com>
2018-12-18 14:49:40 +0300

    fix shadow color

--- src/app/components/app/modal-container/modal/modal.tsx
+++ src/app/components/app/modal-container/modal/modal.tsx
@@ -8,43 +8,7 @@ import { Footer } from '../footer';
 import { MobileHeader } from './mobile-header';
 import { FormContainer } from './form-container';
 import { State } from 'checkout/state';
-import styled, { css } from 'checkout/styled-components';
-import { device } from 'checkout/utils/device';
-
-const FormBlock = styled.div<{ inFrame: boolean }>`
-    position: relative;
-    height: 100%;
-    min-height: 100vh;
-    width: 100%;
-    background-image: ${({ theme }) => theme.gradients.form};
-
-    footer {
-        display: block;
-    }
-
-    @media ${device.desktop} {
-        height: auto;
-        min-height: auto;
-        width: 680px;
-        border-radius: 6px;
-        display: flex;
-        flex-wrap: nowrap;
-        flex-direction: row;
-        padding: 30px;
-        box-sizing: border-box;
-        background-image: ${({ theme }) => theme.gradients.form};
-
-        footer {
-            display: none !important;
-        }
-    }
-
-    ${({ theme, inFrame }) =>
-        inFrame &&
-        css`
-            box-shadow: 0 15px 49px 0 ${theme.color.secondary[0.7]};
-        `};
-`;
+import { FormBlock } from './form-block';
 
 interface ModalDefProps {
     inFrame: boolean;
