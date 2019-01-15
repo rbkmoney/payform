0cc3cd3a3374d85d4e92d10a0c4dc16437d5f9fc
Rinat Arsaev <krickray@gmail.com>
2019-01-15 17:14:03 +0300

    layout-loader to styled

--- src/app/components/app/layout-loader/layout-loader.tsx
+++ src/app/components/app/layout-loader/layout-loader.tsx
@@ -1,11 +1,32 @@
 import * as React from 'react';
-import * as styles from './layout-loader.scss';
-import { Loader } from '../../ui/loader';
 
-const LayoutLoaderDef: React.FC = () => (
-    <div className={styles.loader}>
+import { Loader } from '../../ui/loader';
+import styled from 'checkout/styled-components';
+import { device } from 'checkout/utils/device';
+import { growth } from 'checkout/styled-components/animations';
+
+export const LayoutLoader = styled((props) => (
+    <div {...props}>
         <Loader />
     </div>
-);
+))`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 
-export const LayoutLoader = LayoutLoaderDef;
+    @media ${device.desktop} {
+        position: relative;
+        height: 100%;
+        width: 100%;
+        top: 0;
+        left: 0;
+        display: flex;
+        flex-wrap: nowrap;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        transform: translate(0, 0);
+        animation: ${growth} 0.75s;
+    }
+`;
