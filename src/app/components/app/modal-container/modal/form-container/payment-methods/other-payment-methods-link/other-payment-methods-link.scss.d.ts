7a9ccc1e93dd332b0ff0fd2f17a67f4f9d4947e9
Rinat Arsaev <krickray@gmail.com>
2019-01-21 15:22:03 +0300

    to styled

--- src/app/components/app/modal-container/modal/form-container/payment-methods/other-payment-methods-link/other-payment-methods-link.scss.d.ts
+++
@@ -1,2 +0,0 @@
-export const block: string;
-export const container: string;
--- src/app/components/app/modal-container/modal/form-container/payment-methods/other-payment-methods-link/other-payment-methods-link.tsx
+++ src/app/components/app/modal-container/modal/form-container/payment-methods/other-payment-methods-link/other-payment-methods-link.tsx
@@ -1,8 +1,19 @@
 import * as React from 'react';
 import { MouseEventHandler } from 'react';
+
 import { Locale } from 'checkout/locale';
-import { block, container } from './other-payment-methods-link.scss';
 import { Link } from 'checkout/components/ui/link';
+import styled from 'checkout/styled-components';
+
+const Block = styled.div`
+    display: flex;
+`;
+
+const Container = styled.div`
+    display: inline-block;
+    margin: auto;
+    margin-top: 22px;
+`;
 
 export interface OtherPaymentMethodsLinkProps {
     locale: Locale;
@@ -10,9 +21,9 @@ export interface OtherPaymentMethodsLinkProps {
 }
 
 export const OtherPaymentMethodsLink: React.FC<OtherPaymentMethodsLinkProps> = (props) => (
-    <div className={block}>
-        <div className={container}>
+    <Block>
+        <Container>
             <Link onClick={props.onClick}>{props.locale['form.payment.method.name.others.label']}</Link>
-        </div>
-    </div>
+        </Container>
+    </Block>
 );
