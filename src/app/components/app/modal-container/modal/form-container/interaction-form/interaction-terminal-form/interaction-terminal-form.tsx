6cd4e86fc506f51f8f641d49cf078d811ae1c5fe
Rinat Arsaev <krickray@gmail.com>
2019-01-21 19:08:31 +0300

    to styled

--- src/app/components/app/modal-container/modal/form-container/interaction-form/interaction-terminal-form/interaction-terminal-form.tsx
+++ src/app/components/app/modal-container/modal/form-container/interaction-form/interaction-terminal-form/interaction-terminal-form.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 
 import * as formStyles from '../../form-container.scss';
-import * as styles from './interaction-terminal-form.scss';
 import { State } from 'checkout/state';
 import { Header } from '../../header';
 import { formatInvoiceAmount, FormattedAmount } from 'checkout/utils';
@@ -10,6 +9,15 @@ import { PaymentTerminalReceipt } from 'checkout/backend';
 import { Locale } from 'checkout/locale';
 import { ReceiptInfo } from './receipt-info';
 import { EurosetLogo } from 'checkout/components';
+import styled from 'checkout/styled-components';
+
+const Container = styled.div`
+    display: flex;
+    flex-wrap: nowrap;
+    flex-direction: column;
+    align-content: center;
+    width: 100%;
+`;
 
 const mapStateToProps = (s: State) => ({
     locale: s.config.locale,
@@ -26,11 +34,11 @@ class InteractionTerminalFormDef extends React.Component<InteractionTerminalForm
     render() {
         const { locale, receipt, amount } = this.props;
         return (
-            <div className={styles.container} id="terminal-interaction">
+            <Container id="terminal-interaction">
                 <Header title={this.props.locale['form.header.pay.euroset.label']} />
                 <EurosetLogo className={formStyles.systemLogo} />
                 <ReceiptInfo amount={amount} receipt={receipt} locale={locale} />
-            </div>
+            </Container>
         );
     }
 }
