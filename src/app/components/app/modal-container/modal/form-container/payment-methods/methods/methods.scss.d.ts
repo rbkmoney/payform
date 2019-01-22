add68d5ae8d69b4e282e5c1f977af03857234f01
Rinat Arsaev <krickray@gmail.com>
2019-01-22 15:32:29 +0300

    list to styled

--- src/app/components/app/modal-container/modal/form-container/payment-methods/methods/methods.scss.d.ts
+++ src/app/components/app/modal-container/modal/form-container/payment-methods/methods/methods.scss.d.ts
@@ -1,3 +1,2 @@
-export const list: string;
 export const appear: string;
 export const leave: string;
--- src/app/components/app/modal-container/modal/form-container/payment-methods/methods/methods.tsx
+++ src/app/components/app/modal-container/modal/form-container/payment-methods/methods/methods.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import { CSSTransitionGroup } from 'react-transition-group';
+
 import { FormInfo, PaymentMethod } from 'checkout/state';
 import { Locale } from 'checkout/locale';
 import { getMethods } from './get-methods';
-import { list, appear, leave } from './methods.scss';
+import { appear, leave } from './methods.scss';
 import { PaymentRequestedPayload } from 'checkout/actions';
+import styled from 'checkout/styled-components';
+
+const List = styled(CSSTransitionGroup)`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    min-height: 266px;
+`;
 
 export interface MethodsProps {
     methods: PaymentMethod[];
@@ -18,15 +27,14 @@ export interface MethodsProps {
 export const Methods: React.FC<MethodsProps> = (props) => {
     const { methods, locale, setFormInfo, pay, amountPrefilled, emailPrefilled } = props;
     return (
-        <CSSTransitionGroup
+        <List
             component="ul"
-            className={list}
             transitionName={{ enter: null, appear, leave }}
             transitionEnter={false}
             transitionAppear={true}
             transitionAppearTimeout={1000}
             transitionLeaveTimeout={1000}>
             {getMethods(methods, { locale, setFormInfo, pay, amountPrefilled, emailPrefilled })}
-        </CSSTransitionGroup>
+        </List>
     );
 };
