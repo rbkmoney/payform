5bba0ac264f4a39493bc6e909be0d3774356058d
Rinat Arsaev <krickray@gmail.com>
2019-01-28 14:02:14 +0300

    to styled animation

--- src/app/components/app/modal-container/modal-content/modal-content.tsx
+++ src/app/components/app/modal-container/modal-content/modal-content.tsx
@@ -8,10 +8,54 @@ import { UserInteractionModal } from '../user-interaction-modal';
 import { ModalName, ModalState, State, ModalInteraction } from 'checkout/state';
 import { finishInteraction } from 'checkout/actions';
 import { ModalLoader } from 'checkout/components/app/modal-container/modal-loader';
-import { CSSTransitionGroup } from 'react-transition-group';
 import { Close } from 'checkout/components/app/modal-container/modal/close';
 import styled from 'checkout/styled-components';
 import { device } from 'checkout/utils/device';
+import { stylableTransition, LEAVE, ENTER, LEAVE_ACTIVE, ENTER_ACTIVE } from 'checkout/styled-transition';
+
+const interactionTransitionTime = '0.5s';
+
+const Animation = styled(stylableTransition)`
+    ${ENTER} {
+        opacity: 0;
+
+        @media ${device.desktop} {
+            opacity: 1;
+            transform: perspective(1000px) rotateY(90deg);
+        }
+    }
+
+    ${ENTER_ACTIVE} {
+        opacity: 1;
+        transition: all ${interactionTransitionTime} ease-out;
+
+        @media ${device.desktop} {
+            transform: perspective(1000px) rotateY(0deg);
+            transition-delay: ${interactionTransitionTime};
+        }
+    }
+
+    ${LEAVE} {
+        opacity: 1;
+
+        @media ${device.desktop} {
+            transform: translateY(-50%) perspective(1000px) rotateY(0deg);
+            position: absolute;
+            top: 50%;
+        }
+    }
+
+    ${LEAVE_ACTIVE} {
+        opacity: 0;
+        transition: all ${interactionTransitionTime} ease-in;
+
+        @media ${device.desktop} {
+            opacity: 1;
+            top: 50%;
+            transform: translateY(-50%) perspective(1000px) rotateY(-90deg);
+        }
+    }
+`;
 
 const StyledFooter = styled(Footer)`
     @media ${device.mobile} {
@@ -40,16 +84,12 @@ class ModalContentDef extends React.Component<ModalContentProps> {
             inFrame
         } = this.props;
         return (
-            <CSSTransitionGroup
-                component="div"
-                transitionName="interactionAnimation"
-                transitionEnterTimeout={1000}
-                transitionLeaveTimeout={500}>
+            <Animation enter={1000} leave={500}>
                 <div key={name}>
                     {!inFrame && <Close />}
                     {this.renderContent()}
                 </div>
-            </CSSTransitionGroup>
+            </Animation>
         );
     }
 
