c7c59fc6ce74f2682e29882e0ce6f30492abf022
Rinat Arsaev <krickray@gmail.com>
2019-03-28 19:22:09 +0300

    fix google pay isReadyToPay unfulfilled promise

--- src/app/sagas/initialize-app/initialize-available-payment-methods/bank-card-to-methods/is-ready-to-google-pay.ts
+++ src/app/sagas/initialize-app/initialize-available-payment-methods/bank-card-to-methods/is-ready-to-google-pay.ts
@@ -1,22 +1,30 @@
 import { call, CallEffect } from 'redux-saga/effects';
+
 import { isGooglePaymentClientAvailable } from '../../../../../environment';
 import { logPrefix } from 'checkout/log-messages';
 import { AmountInfoState } from 'checkout/state';
 import { loadThirdPartLib } from './load-third-part-lib';
 
-function isReadyToPay(): Promise<boolean> {
+async function isReadyToPay(): Promise<boolean> {
     if (!isGooglePaymentClientAvailable()) {
-        return Promise.resolve(false);
+        return false;
     }
     const paymentsClient = new google.payments.api.PaymentsClient({ environment: 'PRODUCTION' });
     const allowedPaymentMethods = ['CARD', 'TOKENIZED_CARD'];
-    return paymentsClient
-        .isReadyToPay({ allowedPaymentMethods })
-        .then((res) => res.result)
-        .catch((ex) => {
-            console.error(`${logPrefix} isReadyToGooglePay`, ex);
-            return false;
-        });
+    try {
+        /**
+         * paymentsClient.isReadyToPay promise may not be resolved
+         * (it happened on browsers: Yandex browser for Android in mobile view, some Samsung Internet browsers)
+         */
+        const { result } = (await Promise.race([
+            paymentsClient.isReadyToPay({ allowedPaymentMethods }),
+            new Promise((res, rej) => setTimeout(() => rej(new Error('timeout')), 4000))
+        ])) as ReadyToPayResponse;
+        return result;
+    } catch (e) {
+        console.error(`${logPrefix} isReadyToGooglePay`, e);
+        return false;
+    }
 }
 
 export function* isReadyToGooglePay(amountInfo: AmountInfoState): Iterator<CallEffect | boolean> {
