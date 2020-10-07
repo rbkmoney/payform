5e8f3648568635398ea56075f19180eff28dad19
Rinat Arsaev <krickray@gmail.com>
2020-10-07 16:42:03 +0300

    QPS polling (#338)

--- src/app/components/app/modal-container/modal/form-container/qps-forms/qps-interaction-form.tsx
+++ src/app/components/app/modal-container/modal/form-container/qps-forms/qps-interaction-form.tsx
@@ -2,24 +2,12 @@ import * as React from 'react';
 import { InjectedFormProps, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { bindActionCreators, Dispatch } from 'redux';
-import get from 'lodash-es/get';
 
-import {
-    CardFormValues,
-    FormName,
-    ModalForms,
-    ModalName,
-    ModalState,
-    PaymentMethodName,
-    PaymentStatus,
-    State,
-    TerminalFormValues,
-    InteractionFormInfo
-} from 'checkout/state';
+import { FormName, ModalForms, ModalName, ModalState, State, InteractionFormInfo } from 'checkout/state';
 import { Header } from '../header';
 import { toFieldsConfig } from '../fields-config';
-import { pay, setViewInfoError } from 'checkout/actions';
-import { findNamed, formatAmount } from 'checkout/utils';
+import { setViewInfoError } from 'checkout/actions';
+import { findNamed } from 'checkout/utils';
 import { QRCode } from './qr-code';
 import { QPSInteractionFormProps } from './qps-interaction-form-props';
 import { QrCodeDisplayRequest } from 'checkout/backend';
@@ -35,40 +23,14 @@ const terminalFormInfo = (modals: ModalState[]): InteractionFormInfo => {
 const mapStateToProps = (state: State): Partial<Props> => ({
     terminalFormInfo: terminalFormInfo(state.modals),
     locale: state.config.locale,
-    fieldsConfig: toFieldsConfig(state.config.initConfig, state.model.invoiceTemplate),
-    formValues: get(state.form, 'terminalForm.values'),
-    amount: formatAmount(state.amountInfo)
+    fieldsConfig: toFieldsConfig(state.config.initConfig, state.model.invoiceTemplate)
 });
 
 const mapDispatchToProps = (dispatch: Dispatch<any>): Partial<Props> => ({
-    pay: bindActionCreators(pay, dispatch),
     setViewInfoError: bindActionCreators(setViewInfoError, dispatch)
 });
 
 export class QPSInteractionFormDef extends React.Component<Props> {
-    init(values: TerminalFormValues) {
-        this.props.initialize({
-            email: get(values, 'email'),
-            amount: get(values, 'amount')
-        });
-    }
-
-    componentWillMount() {
-        const {
-            terminalFormInfo: { paymentStatus },
-            formValues
-        } = this.props;
-        this.props.setViewInfoError(false);
-        switch (paymentStatus) {
-            case PaymentStatus.pristine:
-                this.init(formValues);
-                break;
-            case PaymentStatus.needRetry:
-                this.submit(formValues);
-                break;
-        }
-    }
-
     componentWillReceiveProps(props: Props) {
         if (props.submitFailed) {
             props.setViewInfoError(true);
@@ -88,11 +50,6 @@ export class QPSInteractionFormDef extends React.Component<Props> {
             </div>
         );
     }
-
-    private submit = (values: CardFormValues) => {
-        (document.activeElement as HTMLElement).blur();
-        this.props.pay({ method: PaymentMethodName.QPS, values });
-    };
 }
 
 const ReduxForm = reduxForm({
