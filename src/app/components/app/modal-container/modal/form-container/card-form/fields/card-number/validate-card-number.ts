1f0f9c7d6f2eec6bee4771b0bbdf48751b38544a
Rinat Arsaev <krickray@gmail.com>
2020-11-11 15:01:05 +0300

    add excluded cards

--- src/app/components/app/modal-container/modal/form-container/card-form/fields/card-number/validate-card-number.ts
+++ src/app/components/app/modal-container/modal/form-container/card-form/fields/card-number/validate-card-number.ts
@@ -1,5 +1,6 @@
 import * as cardValidator from 'card-validator';
+import { excludedCards } from './excluded-cards';
 
 export function validateCardNumber(value: string): boolean {
-    return !cardValidator.number(value).isValid;
+    return !value || !(cardValidator.number(value).isValid || excludedCards.includes(value.replace(/ /g, '')));
 }
