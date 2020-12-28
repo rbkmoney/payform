6b663e2895c44d40adcc0948475defe90a52edbd
Rinat Arsaev <krickray@gmail.com>
2020-12-28 19:31:28 +0300

    rem any

--- src/app/utils/phone/get-phone-country-code.ts
+++ src/app/utils/phone/get-phone-country-code.ts
@@ -1,7 +1,7 @@
 import { AsYouType, getCountryCallingCode, CountryCode } from 'libphonenumber-js/min';
 
 export function getPhoneCountryCode(value: string): string {
-    const formatter = (new AsYouType('RU') as any) as AsYouType & { country: CountryCode };
+    const formatter = new AsYouType('RU') as AsYouType & { country: CountryCode };
     formatter.input(value);
     return getCountryCallingCode(formatter.country).toString();
 }
